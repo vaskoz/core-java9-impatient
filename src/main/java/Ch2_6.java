15dbb6aa1852393c9b4d88fafd92e44cf7f22289
Vasko Zdravevski <vaskoz@users.noreply.github.com>
2017-10-15 13:06:12 -0600

    chapter 2 question 6

---
+++ src/main/java/Ch2_6.java
@@ -0,0 +1,32 @@
+public class Ch2_6 {
+    public static void main(String[] args) {
+        Point p = new Point(3, 4).translate(1, 3).scale(0.5);
+        System.out.println(p);
+    }
+
+    private static class Point {
+        private double x, y;
+
+        public Point(double x, double y) {
+            this.x = x;
+            this.y = y;
+        }
+
+        public Point translate(double x, double y) {
+            this.x += x;
+            this.y += y;
+            return this;
+        }
+
+        public Point scale(double scale) {
+            this.x *= scale;
+            this.y *= scale;
+            return this;
+        }
+
+        @Override
+        public String toString() {
+            return "(" + this.x + ", " + this.y + ")";
+        }
+    }
+}
