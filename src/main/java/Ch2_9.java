ab59c509ea4a2b94b2c67913a66e2c668a86d3ce
Vasko Zdravevski <vaskoz@users.noreply.github.com>
2017-10-15 13:39:25 -0600

    chapter 2 question 9

---
+++ src/main/java/Ch2_9.java
@@ -0,0 +1,47 @@
+public class Ch2_9 {
+    public static void main(String[] args) {
+        Car c = new Car(35.0);
+        c.drive(100.5);
+        c.refuel(1.5);
+        System.out.printf("Fuel level is %f\n", c.getFuelLevel());
+        System.out.printf("Current position is %f\n", c.getDistance());
+    }
+
+    static class Car {
+        private final double mpg;
+        private double gallons;
+        private double distance = 0.0;
+
+        public Car(double mpg, double gallons) {
+            this.mpg = mpg;
+            this.gallons = gallons;
+        }
+
+        public Car(double mpg) {
+            this(mpg, 10);
+        }
+
+        public void drive(double miles) {
+            double gallonsNeeded = miles / this.mpg;
+            if (gallonsNeeded > this.gallons) {
+                String msg = String.format("Insufficient fuel for this journey. " + "" +
+                        "To drive %f miles you need %f gallons, but only have %f gallons", miles, gallonsNeeded, this.gallons);
+                throw new IllegalArgumentException(msg);
+            }
+            this.distance += miles;
+            this.gallons -= gallonsNeeded;
+        }
+
+        public void refuel(double gallons) {
+            this.gallons += gallons;
+        }
+
+        public double getFuelLevel() {
+            return this.gallons;
+        }
+
+        public double getDistance() {
+            return distance;
+        }
+    }
+}
---
+++ src/test/java/Ch2_9_Test.java
@@ -0,0 +1,62 @@
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class Ch2_9_Test {
+    private static PrintStream originalOut;
+    private ByteArrayOutputStream out;
+    private final String want = "Fuel level is 8.628571\n" +
+            "Current position is 100.500000\n";
+
+    @BeforeAll
+    static void saveOriginals() {
+        originalOut = System.out;
+    }
+
+    @AfterAll
+    static void restoreOriginals() {
+        System.setOut(originalOut);
+    }
+
+    @BeforeEach
+    void setupInput() {
+        this.out = new ByteArrayOutputStream(this.want.length() * 2);
+        System.setOut(new PrintStream(this.out));
+    }
+
+    @Test
+    void check() {
+        Ch2_9.main(null);
+        assertEquals(want.toString(), this.out.toString());
+    }
+
+    @Test
+    void driveFar() {
+        Ch2_9.Car c = new Ch2_9.Car(10, 1);
+        c.drive(9);
+        assertEquals(0.1, c.getFuelLevel(), 0.0001);
+        assertEquals(9.0, c.getDistance(), 0.0001);
+        c.drive(0.99);
+        assertEquals(0.0, c.getFuelLevel(), 0.02);
+        assertEquals(10.0, c.getDistance(), 0.02);
+        c.refuel(10);
+        c.drive(10);
+        assertEquals(9.0, c.getFuelLevel(), 0.02);
+        assertEquals(20.0, c.getDistance(), 0.02);
+    }
+
+    @Test
+    void driveTooFar() {
+        Ch2_9.Car c = new Ch2_9.Car(10, 1);
+        assertThrows(IllegalArgumentException.class, () -> {
+           c.drive(10.1);
+        });
+    }
+}
