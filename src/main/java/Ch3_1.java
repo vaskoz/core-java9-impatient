a49486d239d85aaf0f16fd2490fce28b6f8336ee
Vasko Zdravevski <vaskoz@users.noreply.github.com>
2017-10-23 06:50:47 -0600

    chapter 3 question 1 with parameterized tests

---
+++ src/main/java/Ch3_1.java
@@ -0,0 +1,26 @@
+import java.util.Arrays;
+import java.util.OptionalDouble;
+
+public class Ch3_1 {
+    interface Measurable {
+        double getMeasure();
+    }
+
+    static class Employee implements Measurable {
+        private final double salary;
+
+        public Employee(double salary) {
+            this.salary = salary;
+        }
+
+        @Override
+        public double getMeasure() {
+            return this.salary;
+        }
+
+        static double average(Measurable[] objects) {
+            OptionalDouble result = Arrays.asList(objects).parallelStream().mapToDouble((o)->o.getMeasure()).average();
+            return result.getAsDouble();
+        }
+    }
+}
---
+++ src/test/java/Ch3_1_Test.java
@@ -0,0 +1,19 @@
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import java.util.Arrays;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class Ch3_1_Test {
+    @ParameterizedTest
+    @CsvSource({"'0.0,20.0', 10.0," +
+            "'10.0,20.0,30.0', 20.0" +
+            "'1.0,1.0,1.0,3.0,3.0,3.0', 2.0"})
+    public void testEmployeeAverageSalary(String data, double average) {
+        Ch3_1.Measurable[] input = Arrays.stream(data.split(","))
+                .map(s->new Ch3_1.Employee(Double.parseDouble(s)))
+                .toArray(Ch3_1.Measurable[]::new);
+        assertEquals(average, Ch3_1.Employee.average(input), 0.0001);
+    }
+}
