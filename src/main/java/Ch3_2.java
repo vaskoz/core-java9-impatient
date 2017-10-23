bdb025fd64c59a8af09b4da2a678b377f481a1bc
Vasko Zdravevski <vaskoz@users.noreply.github.com>
2017-10-23 07:33:03 -0600

    chapter 3 question 2

---
+++ src/main/java/Ch3_2.java
@@ -0,0 +1,38 @@
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.Optional;
+import java.util.OptionalDouble;
+
+public class Ch3_2 {
+    interface Measurable {
+        double getMeasure();
+    }
+
+    static class Employee implements Measurable {
+        private final double salary;
+        private final String name;
+
+        public Employee(String name, double salary) {
+            this.name = name;
+            this.salary = salary;
+        }
+
+        @Override
+        public double getMeasure() {
+            return this.salary;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        static double average(Measurable[] objects) {
+            OptionalDouble result = Arrays.asList(objects).parallelStream().mapToDouble((o)->o.getMeasure()).average();
+            return result.getAsDouble();
+        }
+
+        static Measurable largest(Measurable[] objects) {
+            return Arrays.asList(objects).parallelStream().max(Comparator.comparingDouble(d -> d.getMeasure())).get();
+        }
+    }
+}
---
+++ src/test/java/Ch3_2_Test.java
@@ -0,0 +1,19 @@
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import java.util.Arrays;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class Ch3_2_Test {
+    @ParameterizedTest
+    @CsvSource({"'joe:0.0,jane:20.0','jane'",
+            "'fred:10.0,frank:20.0,fink:30.0','fink'",
+            "'julio:4.0,bill:1.0,bob:1.0,jane:1.0,joe:3.0,fred:3.0,james:3.0','julio'"})
+    public void testEmployeeLargestSalaryName(String data, String expectedLargest) {
+        Ch3_2.Measurable[] input = Arrays.stream(data.split(","))
+                .map((s)->new Ch3_2.Employee(s.split(":")[0], Double.parseDouble(s.split(":")[1])))
+                .toArray(Ch3_2.Measurable[]::new);
+        assertEquals(expectedLargest, ((Ch3_2.Employee)Ch3_2.Employee.largest(input)).getName());
+    }
+}
