89b14086d08cf27334096fd2238fd0b2705010ee
Vasko Zdravevski <vaskoz@users.noreply.github.com>
2017-10-23 21:36:29 -0600

    chapter 3 question 9

---
+++ src/main/java/Ch3_9.java
@@ -0,0 +1,35 @@
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class Ch3_9 {
+    private final ReentrantLock lock = new ReentrantLock();
+
+    public static void main(String... args) throws Exception {
+        Ch3_9 app = new Ch3_9();
+        ExecutorService es = Executors.newFixedThreadPool(2);
+        es.submit(app.new Greeter(10, "tom"));
+        es.submit(app.new Greeter(5, "tim"));
+        es.awaitTermination(500, TimeUnit.MILLISECONDS);
+    }
+
+    private class Greeter implements Runnable {
+        private final int n;
+        private final String target;
+
+        public Greeter(int n, String target) {
+            this.n = n;
+            this.target = target;
+        }
+
+        @Override
+        public void run() {
+            for (int i = 0; i < this.n; i++) {
+                Ch3_9.this.lock.lock();
+                System.out.println("Hello, " + this.target);
+                Ch3_9.this.lock.unlock();
+            }
+        }
+    }
+}
---
+++ src/test/java/Ch3_9_Test.java
@@ -0,0 +1,29 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import java.util.Arrays;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class Ch3_9_Test {
+    private ByteArrayOutputStream out;
+
+    @BeforeEach
+    void setupInput() {
+        this.out = new ByteArrayOutputStream(5000);
+        System.setOut(new PrintStream(this.out));
+    }
+
+    @Test
+    public void testGreeterThreads() throws Exception {
+        Ch3_9.main();
+        String output = this.out.toString();
+        String[] parts = output.split("\n");
+        long toms = Arrays.stream(parts).filter(s -> s.contains("tom")).count();
+        long tims = Arrays.stream(parts).filter(s -> s.contains("tim")).count();
+        assertEquals(10, toms);
+        assertEquals(5, tims);
+    }
+}
