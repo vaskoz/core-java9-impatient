55251096ca33c8273326aa61f6e3571310460b10
Vasko Zdravevski <vaskoz@users.noreply.github.com>
2017-10-23 20:38:01 -0600

    chapter 3 question 8

---
+++ src/main/java/Ch3_8.java
@@ -0,0 +1,20 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+
+public class Ch3_8 {
+    public static void luckySort(ArrayList<String> strings, Comparator<String> comp) {
+        while (!isSorted(strings, comp)) {
+            Collections.shuffle(strings);
+        }
+    }
+
+    static boolean isSorted(ArrayList<String> strings, Comparator<String> comp) {
+        for (int i = 0; i < strings.size()-1; i++) {
+            if (comp.compare(strings.get(i), strings.get(i+1)) > 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
---
+++ src/test/java/Ch3_8_Test.java
@@ -0,0 +1,32 @@
+import org.junit.jupiter.api.Test;
+
+import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class Ch3_8_Test {
+    @Test
+    public void testLuckySort() {
+        Comparator<String> cmp = Comparator.naturalOrder();
+        ArrayList<String> data = new ArrayList<>(Arrays.asList("bird", "ant", "door", "cat", "zebra", "everything"));
+        Ch3_8.luckySort(data, cmp);
+        assertTrue(Ch3_8.isSorted(data, cmp));
+    }
+
+    @Test
+    public void testIsSorted() {
+        Comparator<String> cmp = Comparator.naturalOrder();
+        ArrayList<String> notSorted = new ArrayList<>(Arrays.asList("bird", "ant", "door", "cat", "zebra", "everything"));
+        assertFalse(Ch3_8.isSorted(notSorted, cmp));
+
+        notSorted.sort(cmp);
+        assertTrue(Ch3_8.isSorted(notSorted, cmp));
+
+    }
+}
