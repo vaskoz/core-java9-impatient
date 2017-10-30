c4d3630be5d93cdd82beb9072f352c77b9b28a99
Vasko Zdravevski <vaskoz@users.noreply.github.com>
2017-10-29 23:41:50 -0600

    cleanup unused imports

--- src/test/java/Ch4_4_Test.java
+++ src/test/java/Ch4_4_Test.java
@@ -1,6 +1,5 @@
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
