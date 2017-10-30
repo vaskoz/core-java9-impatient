66a81d862b8b464f7374b3589bea095fe4fefc32
Vasko Zdravevski <vaskoz@users.noreply.github.com>
2017-10-30 00:21:37 -0600

    chapter 4 question 7

---
+++ src/main/java/Ch4_7.java
@@ -0,0 +1,17 @@
+public class Ch4_7 {
+    enum PrimaryColors {
+        BLACK, RED, BLUE, GREEN, CYAN, MAGENTA, YELLOW, WHITE;
+
+        public static PrimaryColors getRed() {
+            return RED;
+        }
+
+        public static PrimaryColors getGreen() {
+            return GREEN;
+        }
+
+        public static PrimaryColors getBlue() {
+            return BLUE;
+        }
+    }
+}
