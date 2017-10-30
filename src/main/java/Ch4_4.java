234478c3a128187c53e3669792e31f0779d1b788
Vasko Zdravevski <vaskoz@users.noreply.github.com>
2017-10-29 23:38:08 -0600

    chapter 4 question 4

---
+++ src/main/java/Ch4_4.java
@@ -0,0 +1,118 @@
+public class Ch4_4 {
+    class Point {
+        private final double x, y;
+
+        public Point(double x, double y) {
+            this.x = x;
+            this.y = y;
+        }
+
+        public double getX() {
+            return x;
+        }
+
+        public double getY() {
+            return y;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+
+            Point point = (Point) o;
+
+            if (Double.compare(point.x, x) != 0) return false;
+            return Double.compare(point.y, y) == 0;
+        }
+
+        @Override
+        public int hashCode() {
+            int result;
+            long temp;
+            temp = Double.doubleToLongBits(x);
+            result = (int) (temp ^ (temp >>> 32));
+            temp = Double.doubleToLongBits(y);
+            result = 31 * result + (int) (temp ^ (temp >>> 32));
+            return result;
+        }
+    }
+
+    abstract class Shape {
+        protected Point point;
+
+        public Shape(Point point) {
+            this.point = point;
+        }
+
+        public void moveBy(double dx, double dy) {
+            this.point = new Point(this.point.getX() + dx, this.point.getY() + dy);
+        }
+
+        public abstract Point getCenter();
+    }
+
+    class Circle extends Shape {
+        private final double radius;
+
+        public Circle(Point center, double radius) {
+            super(center);
+            this.radius = radius;
+        }
+
+        @Override
+        public Point getCenter() {
+            return this.point;
+        }
+    }
+
+    class Rectangle extends Shape {
+        private final double width;
+        private final double height;
+
+        public Rectangle(Point topLeft, double width, double height) {
+            super(topLeft);
+            this.width = width;
+            this.height = height;
+        }
+
+        /**
+         * This is how you calculate the center of a rectangle when you have the top-left point.
+         *
+         * @return the center point
+         */
+        @Override
+        public Point getCenter() {
+            double newX = (2 * this.point.getX() + this.width) / 2; // go positive X (right from left)
+            double newY = (2 * this.point.getY() - this.height) / 2; // go negative Y (down from top)
+            return new Point(newX, newY);
+        }
+    }
+
+    class Line extends Shape {
+        private Point from;
+        private Point to;
+
+        public Line(Point from, Point to) {
+            super(null);
+            this.from = from;
+            this.to = to;
+            double centerX = (this.from.getX() + this.to.getX()) / 2;
+            double centerY = (this.from.getY() + this.to.getY()) / 2;
+            this.point = new Point(centerX, centerY);
+        }
+
+        @Override
+        public Point getCenter() {
+            return this.point;
+        }
+
+        @Override
+        public void moveBy(double dx, double dy) {
+            super.moveBy(dx, dy);
+            this.from = new Point(this.from.getX() + dx, this.from.getY() + dy);
+            this.to = new Point(this.to.getX() + dx, this.to.getY() + dy);
+        }
+    }
+
+}
---
+++ src/test/java/Ch4_4_Test.java
@@ -0,0 +1,57 @@
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class Ch4_4_Test {
+    private static Ch4_4 app;
+
+    @BeforeAll
+    public static void setup() {
+        app = new Ch4_4();
+    }
+
+    @AfterAll
+    public static void teardown() {
+        app = null;
+    }
+
+    @ParameterizedTest
+    @CsvSource({"0.0, 0.0, 1.0, 4.0, 4.0, 4.0, 4.0",
+            "1.0, 5.0, 0.5, -1.0, -5.0, 0.0, 0.0",
+            "-1.0, -1.0, 0.1, 2.0, 2.0, 1.0, 1.0",
+            "-5.0, 5.0, 5.0, 10.0, -10.0, 5.0, -5.0"})
+    public void testCircle(double centerX, double centerY, double radius, double moveX, double moveY, double expectedX, double expectedY) {
+        Ch4_4.Point center = app.new Point(centerX, centerY);
+        Ch4_4.Circle circle = app.new Circle(center, radius);
+        circle.moveBy(moveX, moveY);
+        Ch4_4.Point newCenter = circle.getCenter();
+        assertEquals(app.new Point(expectedX, expectedY), newCenter);
+    }
+
+    @ParameterizedTest
+    @CsvSource({"0.0, 0.0, 4.0, 4.0, 2.0, 2.0, 4.0, 0.0",
+            "-5.0, 5.0, 5.0, 10.0, 1.0, 1.0, -1.5, 1.0"})
+    public void testRectangle(double topleftX, double topleftY, double width, double height, double moveX, double moveY, double expectedX, double expectedY) {
+        Ch4_4.Point topleft = app.new Point(topleftX, topleftY);
+        Ch4_4.Rectangle rectangle = app.new Rectangle(topleft, width, height);
+        rectangle.moveBy(moveX, moveY);
+        Ch4_4.Point newCenter = rectangle.getCenter();
+        assertEquals(app.new Point(expectedX, expectedY), newCenter);
+    }
+
+    @ParameterizedTest
+    @CsvSource({"0.0, 0.0, 4.0, 0.0, 2.0, 0.0, 4.0, 0.0",
+            "-1.0, -1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0"})
+    public void testLine(double fromX, double fromY, double toX, double toY, double moveX, double moveY, double expectedX, double expectedY) {
+        Ch4_4.Point from = app.new Point(fromX, fromY);
+        Ch4_4.Point to = app.new Point(toX, toY);
+        Ch4_4.Line line = app.new Line(from, to);
+        line.moveBy(moveX, moveY);
+        Ch4_4.Point newCenter = line.getCenter();
+        assertEquals(app.new Point(expectedX, expectedY), newCenter);
+    }
+}
