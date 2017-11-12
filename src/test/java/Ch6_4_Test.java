11dec8f226563089f149830e8a80806c558d95b8
Vasko Zdravevski <vaskoz@users.noreply.github.com>
2017-11-11 22:08:19 -0700

    chapter 6 question 4

---
+++ src/test/java/Ch6_4_Test.java
@@ -0,0 +1,26 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
+
+public class Ch6_4_Test {
+    @Test
+    public void testTable() {
+        Ch6_4 app = new Ch6_4();
+        Ch6_4.Table<String, Integer> table = app.new Table<>();
+        table.set("foo", 1);
+        table.set("bar", 2);
+        table.set("baz", 3);
+        assertEquals(3, (int) table.get("baz"));
+        table.remove("baz");
+        assertNull(table.get("baz"));
+        assertEquals(2, (int) table.get("bar"));
+        table.remove("bar");
+        assertNull(table.get("bar"));
+        assertEquals(1, (int) table.get("foo"));
+        table.remove("foo");
+        assertNull(table.get("foo"));
+        assertNull(table.get("bar"));
+        assertNull(table.get("baz"));
+    }
+}
