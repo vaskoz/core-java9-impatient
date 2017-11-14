dbd0e51bb364e33efe29f72393eb27968b596379
Vasko Zdravevski <vaskoz@users.noreply.github.com>
2017-11-13 21:17:08 -0700

    chapter 6 questions 7-11 inclusive

---
+++ src/main/java/Ch6_11.java
@@ -0,0 +1,61 @@
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class Ch6_11 {
+    // Question 6.7
+    static class Pair<E extends Comparable<E>> {
+        private E first, second;
+
+        public E getFirst() {
+            return first;
+        }
+
+        public void setFirst(E first) {
+            this.first = first;
+        }
+
+        public E getSecond() {
+            return second;
+        }
+
+        public void setSecond(E second) {
+            this.second = second;
+        }
+
+        // Question 6.8
+        public E max() {
+            return this.first.compareTo(this.second) > 0 ? this.first : this.second;
+        }
+
+        public E min() {
+            return this.first.compareTo(this.second) <= 0 ? this.first : this.second;
+        }
+    }
+
+    // Question 6.9
+    static class Arrays {
+        public static <E extends Comparable<E>> Pair<E> firstLast(ArrayList<? extends E> a) {
+            Pair<E> p = new Pair<>();
+            p.setFirst(a.get(0));
+            p.setSecond(a.get(a.size()-1));
+            return p;
+        }
+
+        // Question 6.10
+        public static <E extends Comparable<E>> E min(ArrayList<E> a) {
+            return Collections.min(a);
+        }
+
+        public static <E extends Comparable<E>> E max(ArrayList<E> a) {
+            return Collections.max(a);
+        }
+
+        // Question 6.11
+        public static <E extends Comparable<E>> Pair<E> minMax(ArrayList<E> a) {
+            Pair<E> p = new Pair<>();
+            p.setFirst(min(a));
+            p.setSecond(max(a));
+            return p;
+        }
+    }
+}
---
+++ src/test/java/Ch6_11_Test.java
@@ -0,0 +1,40 @@
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class Ch6_11_Test {
+    @Test
+    public void testPairMinMax() {
+        Ch6_11.Pair<Double> pair = new Ch6_11.Pair<>();
+        pair.setFirst(4.5);
+        pair.setSecond(3.5);
+        assertEquals(Double.valueOf(3.5), pair.min());
+        assertEquals(Double.valueOf(4.5), pair.max());
+    }
+
+    @Test
+    public void testArraysFirstLast() {
+        ArrayList<Double> ad = new ArrayList<>(Arrays.asList(4.5, 6.3, 2.7, 9.1));
+        Ch6_11.Pair<Double> pd = Ch6_11.Arrays.firstLast(ad);
+        assertEquals(Double.valueOf(4.5), pd.min());
+        assertEquals(Double.valueOf(9.1), pd.max());
+    }
+
+    @Test
+    public void testArraysMinAndMax() {
+        ArrayList<Double> ad = new ArrayList<>(Arrays.asList(4.5, 6.3, 2.7, 9.1));
+        assertEquals(Double.valueOf(2.7), Ch6_11.Arrays.min(ad));
+        assertEquals(Double.valueOf(9.1), Ch6_11.Arrays.max(ad));
+    }
+
+    @Test
+    public void testArraysMinMax() {
+        ArrayList<Double> ad = new ArrayList<>(Arrays.asList(4.5, 6.3, 2.7, 9.1));
+        Ch6_11.Pair<Double> pd = Ch6_11.Arrays.minMax(ad);
+        assertEquals(Double.valueOf(2.7), pd.min());
+        assertEquals(Double.valueOf(9.1), pd.max());
+    }
+}
