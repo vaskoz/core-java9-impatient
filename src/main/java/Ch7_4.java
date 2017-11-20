d176f328b7851f8520b59b84666062109006ee40
Vasko Zdravevski <vaskoz@users.noreply.github.com>
2017-11-19 21:28:03 -0700

    chapter 7 question 4

---
+++ src/main/java/Ch7_4.java
@@ -0,0 +1,29 @@
+import java.util.List;
+import java.util.ListIterator;
+
+public class Ch7_4 {
+    public static <E> void removeTargetFor(List<E> list, E target) {
+        for (E e : list) {
+            if (e.equals(target)) {
+                list.remove(e); // throws ConcurrentModificationException
+            }
+        }
+    }
+
+    public static <E> void removeTargetIterator(List<E> list, E target) {
+        for (ListIterator<E> iterator = list.listIterator(); iterator.hasNext(); ) {
+            E e = iterator.next();
+            if (e.equals(target)) {
+                iterator.remove();
+            }
+        }
+    }
+
+    public static <E> void removeTargetCollection(List<E> list, E target) {
+        list.remove(target);
+    }
+
+    public static <E> void removeTargetIf(List<E> list, E target) {
+        list.removeIf(e -> e.equals(target));
+    }
+}
---
+++ src/test/java/Ch7_4_Test.java
@@ -0,0 +1,38 @@
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.ConcurrentModificationException;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class Ch7_4_Test {
+    private static List<String> expected = List.of("a", "b");
+    private List<String> input = new ArrayList<>(Arrays.asList("a", "b", "c"));
+    private String target = "c";
+
+    @Test
+    public void testRemoveTargetFor() {
+        assertThrows(ConcurrentModificationException.class, () -> Ch7_4.removeTargetFor(this.input, this.target));
+    }
+
+    @Test
+    public void testRemoveTargetIterator() {
+        Ch7_4.removeTargetIterator(this.input, this.target);
+        assertEquals(expected, this.input);
+    }
+
+    @Test
+    public void testRemoveTargetCollection() {
+        Ch7_4.removeTargetCollection(this.input, this.target);
+        assertEquals(expected, this.input);
+    }
+
+    @Test
+    public void testRemoveTargetIf() {
+        Ch7_4.removeTargetIf(this.input, this.target);
+        assertEquals(expected, this.input);
+    }
+}
