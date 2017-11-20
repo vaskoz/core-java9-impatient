481ee35641ac0c43592eccd61b8d84664b4e63ca
Vasko Zdravevski <vaskoz@users.noreply.github.com>
2017-11-19 22:30:10 -0700

    chapter 7 question 6

---
+++ src/main/java/Ch7_6.java
@@ -0,0 +1,8 @@
+import java.util.Map;
+import java.util.Set;
+
+public class Ch7_6 {
+    public static <AS extends Set<Integer>> void someMethod(Map<String, AS> input) {
+        // testing compilation only
+    }
+}
---
+++ src/test/java/Ch7_6_Test.java
@@ -0,0 +1,13 @@
+import org.junit.jupiter.api.Test;
+
+import java.util.HashMap;
+import java.util.HashSet;
+
+public class Ch7_6_Test {
+    @Test
+    public void testSomeMethod() {
+        HashMap<String, HashSet<Integer>> input = new HashMap<>();
+        Ch7_6.someMethod(input);
+        // assert that it compiles
+    }
+}
