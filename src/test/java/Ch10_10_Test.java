798184000a25e99412913fa6ebab1d0d241098d6
Vasko Zdravevski <vaskoz@users.noreply.github.com>
2017-12-14 07:14:37 -0700

    chapter 10 question 10

---
+++ src/test/java/Ch10_10_Test.java
@@ -0,0 +1,28 @@
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+import java.util.concurrent.atomic.LongAccumulator;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class Ch10_10_Test {
+    @Test
+    public void longAccumulatorMinimum() {
+        LongAccumulator minimum = new LongAccumulator((left, right) ->
+                left < right ? left : right, 0L);
+        for (Integer i : List.of(10, 3, 20, 100, -2)) {
+            minimum.accumulate(i);
+        }
+        assertEquals(-2, minimum.get());
+    }
+
+    @Test
+    public void longAccumulatorMaximum() {
+        LongAccumulator maximum = new LongAccumulator((left, right) ->
+                left > right ? left : right, 0L);
+        for (Integer i : List.of(10, 3, 20, 100, -2)) {
+            maximum.accumulate(i);
+        }
+        assertEquals(100, maximum.get());
+    }
+}
