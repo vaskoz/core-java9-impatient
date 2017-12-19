9d9094e765acb59b51240130847f9f38e414e1ec
Vasko Zdravevski <vaskoz@users.noreply.github.com>
2017-12-18 20:48:25 -0700

    chapter 2 question 17

---
+++ src/main/java/Ch2_17.java
@@ -0,0 +1,60 @@
+public class Ch2_17 {
+    static class Queue {
+        private Node head;
+        private Node tail;
+
+        public void add(String s) {
+            Node n = new Node(s);
+            if (this.head == null) {
+                this.head = n;
+                this.tail = n;
+            } else {
+                this.tail.next = n;
+                this.tail = n;
+            }
+        }
+
+        public String remove() {
+            if (this.head == null) {
+                return null;
+            } else {
+                Node n = this.head;
+                this.head = this.head.next;
+                return n.getData();
+            }
+        }
+
+        public java.util.Iterator<String> iterator() {
+            return new Iterator();
+        }
+
+        private class Node {
+            private final String data;
+            Node prev, next;
+
+            public Node(String data) {
+                this.data = data;
+            }
+
+            public String getData() {
+                return data;
+            }
+        }
+
+        private class Iterator implements java.util.Iterator<String> {
+            private Node current = head;
+
+            @Override
+            public boolean hasNext() {
+                return current != null;
+            }
+
+            @Override
+            public String next() {
+                String result = hasNext() ? current.getData() : null;
+                this.current = hasNext() ? this.current.next : null;
+                return result;
+            }
+        }
+    }
+}
---
+++ src/test/java/Ch2_17_Test.java
@@ -0,0 +1,43 @@
+import org.junit.jupiter.api.Test;
+
+import java.util.Iterator;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class Ch2_17_Test {
+    @Test
+    public void testQueueIterator() {
+        Ch2_17.Queue q = new Ch2_17.Queue();
+        q.add("a");
+        q.add("b");
+        q.add("c");
+        q.add("hi");
+        q.add("bye");
+        q.add("there");
+        Iterator<String> iter = q.iterator();
+        assertTrue(iter.hasNext());
+        assertEquals("a", iter.next());
+        assertTrue(iter.hasNext());
+        assertEquals("b", iter.next());
+        assertTrue(iter.hasNext());
+        assertEquals("c", iter.next());
+        assertTrue(iter.hasNext());
+        assertEquals("hi", iter.next());
+        assertTrue(iter.hasNext());
+        assertEquals("bye", iter.next());
+        assertTrue(iter.hasNext());
+        assertEquals("there", iter.next());
+        assertFalse(iter.hasNext());
+
+        assertEquals("a", q.remove());
+        assertEquals("b", q.remove());
+        assertEquals("c", q.remove());
+        assertEquals("hi", q.remove());
+        assertEquals("bye", q.remove());
+        assertEquals("there", q.remove());
+        assertNull(q.remove());
+    }
+}
