7d774389cc57b2cd0fe1ff5522bda3e667e2d3b7
Vasko Zdravevski <vaskoz@users.noreply.github.com>
2017-12-18 20:35:17 -0700

    chapter 2 question 16

---
+++ src/main/java/Ch2_16.java
@@ -0,0 +1,40 @@
+public class Ch2_16 {
+    static class Queue {
+        private Node head;
+        private Node tail;
+
+        public void add(String s) {
+            Node n = new Node(s);
+            if (this.head == null) {
+                this.head = n;
+                this.tail = n;
+            } else {
+                this.tail.next = n;
+                this.tail = n;
+            }
+        }
+
+        public String remove() {
+            if (this.head == null) {
+                return null;
+            } else {
+                Node n = this.head;
+                this.head = this.head.next;
+                return n.getData();
+            }
+        }
+
+        private class Node {
+            private final String data;
+            Node prev, next;
+
+            public Node(String data) {
+                this.data = data;
+            }
+
+            public String getData() {
+                return data;
+            }
+        }
+    }
+}
---
+++ src/test/java/Ch2_16_Test.java
@@ -0,0 +1,24 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
+
+public class Ch2_16_Test {
+    @Test
+    public void testQueue() {
+        Ch2_16.Queue q = new Ch2_16.Queue();
+        q.add("a");
+        q.add("b");
+        q.add("c");
+        q.add("hi");
+        assertEquals("a", q.remove());
+        assertEquals("b", q.remove());
+        assertEquals("c", q.remove());
+        q.add("bye");
+        q.add("there");
+        assertEquals("hi", q.remove());
+        assertEquals("bye", q.remove());
+        assertEquals("there", q.remove());
+        assertNull(q.remove());
+    }
+}
