bba2dd49cea72e23b1591b86a04035dd1af262ee
Vasko Zdravevski <vaskoz@users.noreply.github.com>
2017-12-20 15:08:06 -0700

    chapter 4 question 10

---
+++ src/main/java/Ch4_10.java
@@ -0,0 +1,17 @@
+import java.io.PrintStream;
+import java.lang.reflect.Method;
+import java.lang.reflect.Modifier;
+import java.util.Arrays;
+
+public class Ch4_10 {
+    public static void printMembers(PrintStream out, Class clazz) {
+        while (clazz != null) {
+            for (Method m : clazz.getDeclaredMethods()) {
+                out.println(Modifier.toString(m.getModifiers()) + " "
+                        + m.getReturnType().getCanonicalName() + " "
+                        + m.getName() + Arrays.toString(m.getParameters()));
+            }
+            clazz = clazz.getSuperclass();
+        }
+    }
+}
---
+++ src/test/java/Ch4_10_Test.java
@@ -0,0 +1,37 @@
+import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class Ch4_10_Test {
+    String expected = "protected void finalize[]\n" +
+            "public final void wait[long arg0, int arg1]\n" +
+            "public final void wait[]\n" +
+            "public final native void wait[long arg0]\n" +
+            "public boolean equals[java.lang.Object arg0]\n" +
+            "public java.lang.String toString[]\n" +
+            "public native int hashCode[]\n" +
+            "public final native java.lang.Class getClass[]\n" +
+            "protected native java.lang.Object clone[]\n" +
+            "public final native void notify[]\n" +
+            "public final native void notifyAll[]\n" +
+            "private static native void registerNatives[]\n";
+
+    @Test
+    public void testMembersOfIntArray() {
+        Class clazz = int[].class;
+        ByteArrayOutputStream out = new ByteArrayOutputStream(5000);
+        Ch4_10.printMembers(new PrintStream(out), clazz);
+        assertEquals(expected, out.toString());
+    }
+
+    @Test
+    public void testMembersOfObject() {
+        Class clazz = Object.class;
+        ByteArrayOutputStream out = new ByteArrayOutputStream(5000);
+        Ch4_10.printMembers(new PrintStream(out), clazz);
+        assertEquals(expected, out.toString());
+    }
+}
