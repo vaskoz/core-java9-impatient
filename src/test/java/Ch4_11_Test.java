95ce90a5d355eb8399e7b71157f4ca98810ad5a8
Vasko Zdravevski <vaskoz@users.noreply.github.com>
2017-12-21 07:54:11 -0700

    chapter 4 question 11

---
+++ src/test/java/Ch4_11_Test.java
@@ -0,0 +1,30 @@
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class Ch4_11_Test {
+    private PrintStream realStdOut;
+
+    @BeforeEach
+    public void setupStandardOut() {
+        this.realStdOut = System.out;
+    }
+
+    @AfterEach
+    public void restoreStandardOut() {
+        System.setOut(this.realStdOut);
+    }
+
+    @Test
+    public void reflectiveHelloWorld() {
+        ByteArrayOutputStream out = new ByteArrayOutputStream(100);
+        System.setOut(new PrintStream(out));
+        Ch4_11.main();
+        assertEquals(Ch4_11.message + "\n", out.toString());
+    }
+}
