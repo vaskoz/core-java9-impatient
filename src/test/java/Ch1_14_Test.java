f66b715e2b6ed759c6a60f96ba35c1d5ce4fb590
Vasko Zdravevski <vaskoz@users.noreply.github.com>
2017-12-21 16:13:35 -0700

    get 100% test coverage on ch1q14

--- src/test/java/Ch1_14_Test.java
+++ src/test/java/Ch1_14_Test.java
@@ -1,24 +1,32 @@
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.InputStream;
 import java.io.PrintStream;
-import java.util.ArrayList;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class Ch1_14_Test {
     private static PrintStream originalOut;
     private static InputStream originalIn;
-    private final String input = "16 3 2 13\n" +
+    private final String magicInput = "16 3 2 13\n" +
             "5 10 11 8\n" +
             "9 6 7 12\n" +
             "4 15 14 1\n\n";
-    private final String want = "Is Magic?: true\n";
+    private final String notMagicRow = "17 3 2 13\n" +
+            "5 10 11 8\n" +
+            "9 6 7 12\n" +
+            "4 15 14 1\n\n";
+    ;
+    private final String notMagicColumn = "16 3 2 13\n" +
+            "5 10 11 8\n" +
+            "9 6 7 12\n" +
+            "5 15 14 1\n\n";
+    ;
+    private final String want = "Is Magic?: ";
     private ByteArrayOutputStream out;
 
     @BeforeAll
@@ -33,17 +41,34 @@ public class Ch1_14_Test {
         System.setIn(originalIn);
     }
 
-    @BeforeEach
-    void setupInput() {
-        InputStream in = new ByteArrayInputStream(input.getBytes());
+    @Test
+    void check() {
+        InputStream in = new ByteArrayInputStream(magicInput.getBytes());
         this.out = new ByteArrayOutputStream(this.want.length() * 2);
         System.setOut(new PrintStream(this.out));
         System.setIn(in);
+        Ch1_14.main(null);
+        assertEquals(this.want + "true\n", this.out.toString());
     }
 
     @Test
-    void check() {
+    void checkNotMagicRow() {
+        InputStream in = new ByteArrayInputStream(notMagicRow.getBytes());
+        this.out = new ByteArrayOutputStream(this.want.length() * 2);
+        System.setOut(new PrintStream(this.out));
+        System.setIn(in);
         Ch1_14.main(null);
-        assertEquals(this.want, this.out.toString());
+        assertEquals(this.want + "false\n", this.out.toString());
     }
+
+    @Test
+    void checkNotMagicColumn() {
+        InputStream in = new ByteArrayInputStream(notMagicColumn.getBytes());
+        this.out = new ByteArrayOutputStream(this.want.length() * 2);
+        System.setOut(new PrintStream(this.out));
+        System.setIn(in);
+        Ch1_14.main(null);
+        assertEquals(this.want + "false\n", this.out.toString());
+    }
+
 }
