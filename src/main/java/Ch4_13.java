68a769eeabdfbe135e4ba0baaa4b80f09dabc9c9
Vasko Zdravevski <vaskoz@users.noreply.github.com>
2017-12-22 11:20:43 -0700

    chapter 4 question 13

---
+++ src/main/java/Ch4_13.java
@@ -0,0 +1,21 @@
+import java.io.PrintStream;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.lang.reflect.Modifier;
+
+public class Ch4_13 {
+    public static void printTableStaticDoubleValues(PrintStream out, Method method,
+                                                    double lower, double upper, double step)
+            throws IllegalAccessException, InvocationTargetException {
+        if (method == null || !Modifier.isStatic(method.getModifiers())
+                || method.getParameterTypes().length != 1
+                || !(method.getParameterTypes()[0].equals(double.class) && !method.getParameterTypes()[0].equals(Double.class))) {
+            throw new IllegalArgumentException("Method must be of form AnyType SomeMethod(OneDouble)");
+        }
+        out.println(method.getDeclaringClass().getCanonicalName() + "." + method.getName());
+        for (double value = lower; value <= upper; value += step) {
+            Object result = method.invoke(method.getDeclaringClass(), value);
+            out.println("Value: " + value + " returns: " + result);
+        }
+    }
+}
---
+++ src/test/java/Ch4_13_Test.java
@@ -0,0 +1,70 @@
+import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class Ch4_13_Test {
+    @Test
+    public void testMathSqrt() {
+        String want = "java.lang.Math.sqrt\n" +
+                "Value: 4.0 returns: 2.0\n" +
+                "Value: 6.0 returns: 2.449489742783178\n" +
+                "Value: 8.0 returns: 2.8284271247461903\n" +
+                "Value: 10.0 returns: 3.1622776601683795\n" +
+                "Value: 12.0 returns: 3.4641016151377544\n" +
+                "Value: 14.0 returns: 3.7416573867739413\n" +
+                "Value: 16.0 returns: 4.0\n";
+        ByteArrayOutputStream out = new ByteArrayOutputStream(1000);
+        try {
+            Method sqrtMethod = Math.class.getMethod("sqrt", double.class);
+            Ch4_13.printTableStaticDoubleValues(new PrintStream(out), sqrtMethod, 4.0, 16.0, 2.0);
+        } catch (NoSuchMethodException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (InvocationTargetException e) {
+            e.printStackTrace();
+        }
+        assertEquals(want, out.toString());
+    }
+
+    @Test
+    public void testDoubleToHexString() {
+        String want = "java.lang.Double.toHexString\n" +
+                "Value: 4.0 returns: 0x1.0p2\n" +
+                "Value: 6.0 returns: 0x1.8p2\n" +
+                "Value: 8.0 returns: 0x1.0p3\n" +
+                "Value: 10.0 returns: 0x1.4p3\n" +
+                "Value: 12.0 returns: 0x1.8p3\n" +
+                "Value: 14.0 returns: 0x1.cp3\n" +
+                "Value: 16.0 returns: 0x1.0p4\n";
+        ByteArrayOutputStream out = new ByteArrayOutputStream(1000);
+        try {
+            Method toHexString = Double.class.getMethod("toHexString", double.class);
+            Ch4_13.printTableStaticDoubleValues(new PrintStream(out), toHexString, 4.0, 16.0, 2.0);
+        } catch (NoSuchMethodException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (InvocationTargetException e) {
+            e.printStackTrace();
+        }
+        assertEquals(want, out.toString());
+    }
+
+    @Test
+    public void testInvalidMethod() {
+        try {
+            Method m = String.class.getMethod("toString");
+            assertThrows(IllegalArgumentException.class, () ->
+                    Ch4_13.printTableStaticDoubleValues(null, m, 0.0, 1.0, 1.0));
+        } catch (NoSuchMethodException e) {
+            e.printStackTrace();
+        }
+    }
+}
