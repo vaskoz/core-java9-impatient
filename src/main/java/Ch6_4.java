9b6aa3388aeee34e4722e3f75c3475461edde572
Vasko Zdravevski <vaskoz@users.noreply.github.com>
2017-12-23 13:14:13 -0700

    improve coverage ch6q4

--- src/main/java/Ch6_4.java
+++ src/main/java/Ch6_4.java
@@ -57,10 +57,6 @@ public class Ch6_4 {
                 return key;
             }
 
-            public void setKey(K key) {
-                this.key = key;
-            }
-
             public V getValue() {
                 return value;
             }
