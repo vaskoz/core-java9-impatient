c5ab54a08d759278072f0105084a3bae03f5d3c4
Vasko Zdravevski <vaskoz@users.noreply.github.com>
2018-01-02 19:55:21 -0700

    chapter 5 question 11

---
+++ src/main/java/Ch5_11.java
@@ -0,0 +1,19 @@
+import java.math.BigInteger;
+
+public class Ch5_11 {
+    public static BigInteger factorial(int n) {
+        if (n < 0) {
+            throw new IllegalArgumentException("N can't be negative");
+        }
+        if (n == 0 || n == 1) {
+            return BigInteger.ONE;
+        }
+        StackWalker walker = StackWalker.getInstance();
+        walker.forEach(stackFrame -> {
+            if (stackFrame.getClassName().equals("Ch5_11")) {
+                System.out.println(stackFrame);
+            }
+        });
+        return BigInteger.valueOf(n).multiply(factorial(n - 1));
+    }
+}
---
+++ src/test/java/Ch5_11_Test.java
@@ -0,0 +1,80 @@
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import java.math.BigInteger;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class Ch5_11_Test {
+    private PrintStream originalOut;
+    private ByteArrayOutputStream out;
+
+    @BeforeEach
+    public void setup() {
+        this.originalOut = System.out;
+        this.out = new ByteArrayOutputStream(10_000);
+        System.setOut(new PrintStream(this.out));
+    }
+
+    @AfterEach
+    public void teardown() {
+        System.setOut(this.originalOut);
+        this.out = null;
+    }
+
+    @Test
+    public void testFactorial() {
+        assertThrows(IllegalArgumentException.class, () -> Ch5_11.factorial(-1));
+        assertEquals(BigInteger.valueOf(3628800), Ch5_11.factorial(10));
+        String want = "Ch5_11.factorial(Ch5_11.java:12)\n" +
+                "Ch5_11.factorial(Ch5_11.java:12)\n" +
+                "Ch5_11.factorial(Ch5_11.java:17)\n" +
+                "Ch5_11.factorial(Ch5_11.java:12)\n" +
+                "Ch5_11.factorial(Ch5_11.java:17)\n" +
+                "Ch5_11.factorial(Ch5_11.java:17)\n" +
+                "Ch5_11.factorial(Ch5_11.java:12)\n" +
+                "Ch5_11.factorial(Ch5_11.java:17)\n" +
+                "Ch5_11.factorial(Ch5_11.java:17)\n" +
+                "Ch5_11.factorial(Ch5_11.java:17)\n" +
+                "Ch5_11.factorial(Ch5_11.java:12)\n" +
+                "Ch5_11.factorial(Ch5_11.java:17)\n" +
+                "Ch5_11.factorial(Ch5_11.java:17)\n" +
+                "Ch5_11.factorial(Ch5_11.java:17)\n" +
+                "Ch5_11.factorial(Ch5_11.java:17)\n" +
+                "Ch5_11.factorial(Ch5_11.java:12)\n" +
+                "Ch5_11.factorial(Ch5_11.java:17)\n" +
+                "Ch5_11.factorial(Ch5_11.java:17)\n" +
+                "Ch5_11.factorial(Ch5_11.java:17)\n" +
+                "Ch5_11.factorial(Ch5_11.java:17)\n" +
+                "Ch5_11.factorial(Ch5_11.java:17)\n" +
+                "Ch5_11.factorial(Ch5_11.java:12)\n" +
+                "Ch5_11.factorial(Ch5_11.java:17)\n" +
+                "Ch5_11.factorial(Ch5_11.java:17)\n" +
+                "Ch5_11.factorial(Ch5_11.java:17)\n" +
+                "Ch5_11.factorial(Ch5_11.java:17)\n" +
+                "Ch5_11.factorial(Ch5_11.java:17)\n" +
+                "Ch5_11.factorial(Ch5_11.java:17)\n" +
+                "Ch5_11.factorial(Ch5_11.java:12)\n" +
+                "Ch5_11.factorial(Ch5_11.java:17)\n" +
+                "Ch5_11.factorial(Ch5_11.java:17)\n" +
+                "Ch5_11.factorial(Ch5_11.java:17)\n" +
+                "Ch5_11.factorial(Ch5_11.java:17)\n" +
+                "Ch5_11.factorial(Ch5_11.java:17)\n" +
+                "Ch5_11.factorial(Ch5_11.java:17)\n" +
+                "Ch5_11.factorial(Ch5_11.java:17)\n" +
+                "Ch5_11.factorial(Ch5_11.java:12)\n" +
+                "Ch5_11.factorial(Ch5_11.java:17)\n" +
+                "Ch5_11.factorial(Ch5_11.java:17)\n" +
+                "Ch5_11.factorial(Ch5_11.java:17)\n" +
+                "Ch5_11.factorial(Ch5_11.java:17)\n" +
+                "Ch5_11.factorial(Ch5_11.java:17)\n" +
+                "Ch5_11.factorial(Ch5_11.java:17)\n" +
+                "Ch5_11.factorial(Ch5_11.java:17)\n" +
+                "Ch5_11.factorial(Ch5_11.java:17)\n";
+        assertEquals(want, this.out.toString());
+    }
+}
