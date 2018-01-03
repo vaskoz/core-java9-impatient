c2988b12ac4e761f3abb6d0bcc2615b054374046
Vasko Zdravevski <vaskoz@users.noreply.github.com>
2018-01-02 19:25:52 -0700

    chapter 5 question 9

---
+++ src/main/java/Ch5_9.java
@@ -0,0 +1,16 @@
+import java.util.concurrent.locks.ReentrantLock;
+
+public class Ch5_9 {
+    public static AutoCloseableReentrantLock lock() {
+        AutoCloseableReentrantLock reentrantLock = new AutoCloseableReentrantLock();
+        reentrantLock.lock();
+        return reentrantLock;
+    }
+
+    public static class AutoCloseableReentrantLock extends ReentrantLock implements AutoCloseable {
+        @Override
+        public void close() {
+            this.unlock();
+        }
+    }
+}
---
+++ src/test/java/Ch5_9_Test.java
@@ -0,0 +1,16 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class Ch5_9_Test {
+    @Test
+    public void testLock() {
+        Ch5_9.AutoCloseableReentrantLock lock = Ch5_9.lock();
+        try (lock) {
+            assertTrue(lock.isLocked());
+        } finally {
+            assertFalse(lock.isLocked());
+        }
+    }
+}
