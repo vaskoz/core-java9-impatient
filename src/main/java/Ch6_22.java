fab5938a1cce4962e9f4f2b9f716121c2459a9d3
Vasko Zdravevski <vaskoz@users.noreply.github.com>
2018-01-04 07:43:15 -0700

    update chapter 6 question 22 to use constructor reference

--- src/main/java/Ch6_22.java
+++ src/main/java/Ch6_22.java
@@ -1,11 +1,12 @@
 import java.util.concurrent.Callable;
+import java.util.function.Function;
 
 public class Ch6_22 {
-    public static <V, T extends Throwable> V doWork(Callable<V> c) throws T {
+    public static <V, T extends Throwable> V doWork(Callable<V> c, Function<Throwable, T> constructor) throws T {
         try {
             return c.call();
         } catch (Throwable realEx) {
-            throw (T) realEx;
+            throw constructor.apply(realEx);
         }
     }
 }
--- src/test/java/Ch6_22_Test.java
+++ src/test/java/Ch6_22_Test.java
@@ -6,9 +6,12 @@ public class Ch6_22_Test {
     @Test
     public void testDoWork() {
         class MyPersonalException extends Exception {
+            public MyPersonalException(Throwable cause) {
+                super(cause);
+            }
         }
         assertThrows(MyPersonalException.class, () -> Ch6_22.doWork(() -> {
-            throw new MyPersonalException();
-        }));
+            throw new MyPersonalException(null);
+        }, MyPersonalException::new));
     }
 }
