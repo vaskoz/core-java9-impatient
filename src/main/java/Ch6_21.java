bf4f4e4bc723a7835a894e54f0ce4d8def3fbece
Vasko Zdravevski <vaskoz@users.noreply.github.com>
2018-01-05 19:03:12 -0700

    remove unnecessary constructor ch6q21

--- src/main/java/Ch6_21.java
+++ src/main/java/Ch6_21.java
@@ -1,11 +1,10 @@
-import java.util.ArrayList;
 import java.util.Collections;
 
 public class Ch6_21 {
     public static class Arrays {
         @SafeVarargs
         public static <T> T[] construct(int size, T... emptyValue) {
-            return new ArrayList<>(Collections.nCopies(size, null)).toArray(emptyValue);
+            return Collections.nCopies(size, null).toArray(emptyValue);
         }
     }
 }
