f00f8ce80674370ea67aceda0108cda140f581de
Vasko Zdravevski <vaskoz@users.noreply.github.com>
2018-08-31 17:39:43 -0600

    JDK11 has more language/locale combinations than JDK10

--- src/test/java/Ch13_4_Test.java
+++ src/test/java/Ch13_4_Test.java
@@ -5,6 +5,6 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class Ch13_4_Test {
     @Test
     public void getAllLanguages() {
-        assertEquals(16572, Ch13_4.getAllLanguages().size());
+        assertEquals(19161, Ch13_4.getAllLanguages().size());
     }
 }
