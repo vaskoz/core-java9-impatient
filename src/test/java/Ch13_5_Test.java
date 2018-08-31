2e06f94445ecbf9b8fc5879cebd0dc8cd42a693e
Vasko Zdravevski <vaskoz@users.noreply.github.com>
2018-08-31 17:40:52 -0600

    JDK11 has more currency/locale combinations than JDK10

--- src/test/java/Ch13_5_Test.java
+++ src/test/java/Ch13_5_Test.java
@@ -5,6 +5,6 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class Ch13_5_Test {
     @Test
     public void getAllCurrencies() {
-        assertEquals(19885, Ch13_5.getAllCurrencies().size());
+        assertEquals(21241, Ch13_5.getAllCurrencies().size());
     }
 }
