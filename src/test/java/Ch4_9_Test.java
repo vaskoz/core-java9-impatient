2d3ff244eb23978a50c6b0374b039759eaf8525c
Vasko Zdravevski <vaskoz@users.noreply.github.com>
2018-08-31 18:00:18 -0600

    updates for UniversalToString for JDK11

--- src/test/java/Ch4_9_Test.java
+++ src/test/java/Ch4_9_Test.java
@@ -19,7 +19,7 @@ public class Ch4_9_Test {
     }
 
     @ParameterizedTest
-    @CsvSource({"3, 4, 5, 'x:unable to access\ny:4\nz:5\n'"})
+    @CsvSource({"3, 4, 5, 'x:3\ny:4\nz:5\n'"})
     public void testA(int x, int y, int z, String fragment) {
         var a = app.new A(x, y, z);
         var parts = fragment.split("\n");
@@ -30,7 +30,7 @@ public class Ch4_9_Test {
     }
 
     @ParameterizedTest
-    @CsvSource({"'foo', 'bar', 'baz', 'a:unable to access\nb:unable to access\nc:baz\n'"})
+    @CsvSource({"'foo', 'bar', 'baz', 'a:foo\nb:bar\nc:baz\n'"})
     public void testB(String a, String b, String c, String fragment) {
         var bb = app.new B(a, b, c);
         var parts = fragment.split("\n");
