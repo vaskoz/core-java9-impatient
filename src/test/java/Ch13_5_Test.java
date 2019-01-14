020518dbc12eb379001271a5e4b9e2eaa50a7b82
Vasko Zdravevski <vaskoz@users.noreply.github.com>
2019-01-14 16:09:52 -0700

    fix assertion difference in jdk12

--- src/test/java/Ch13_5_Test.java
+++ src/test/java/Ch13_5_Test.java
@@ -5,6 +5,6 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class Ch13_5_Test {
     @Test
     public void getAllCurrencies() {
-        assertEquals(21241, Ch13_5.getAllCurrencies().size());
+        assertEquals(21243, Ch13_5.getAllCurrencies().size());
     }
 }
